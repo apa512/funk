f0a5aea99e057fc6250b04ef216d936bc9710046
Erik Strömberg <erik.stromberg@gmail.com>
2017-05-27 02:41:25 +0200

    Also not used

--- src/Main.hs
+++ src/Main.hs
@@ -6,8 +6,6 @@ module Main where
 import System.Environment (getArgs)
 
 import qualified Funk.CLI as CLI
-import Funk.LastFM
-import Funk.Track
 
 handleArgs args@("status":"playing":_) = CLI.handlePlaying args
 handleArgs _ = putStrLn "?"
